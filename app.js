0b9c06030bea993165864f58b0bed7dcc08c43ca
Barak Merdler <39872010+BarakMerdler@users.noreply.github.com>
2019-09-24 00:33:18 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+function init(){
+
+  var container = document.getElementById("continer");
+  var inner = document.getElementById("layer1");
+  var inner1 =  document.getElementById("layer2");
+  // Mouse
+  var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function(event) {
+      var e = event || window.event;
+      this.x = e.clientX - this._x;
+      this.y = (e.clientY - this._y) * -1;
+    },
+    setOrigin: function(e) {
+      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function() {
+      return "(" + this.x + ", " + this.y + ")";
+    }
+  };
+
+  // Track the mouse position relative to the center of the container.
+  mouse.setOrigin(container);
+
+  //-----------------------------------------
+
+  var counter = 0;
+  var updateRate = 10;
+  var isTimeToUpdate = function() {
+    return counter++ % updateRate === 0;
+  };
+
+  //-----------------------------------------
+
+  var onMouseEnterHandler = function(event) {
+    update(event);
+  };
+
+  var onMouseLeaveHandler = function() {
+    inner.style = "";
+  };
+
+  var onMouseMoveHandler = function(event) {
+    if (isTimeToUpdate()) {
+      update(event);
+    }
+  };
+
+  //-----------------------------------------
+
+  var update = function(event) {
+    mouse.updatePosition(event);
+    updateTransformStyle(
+      (mouse.y / inner.offsetHeight / 2).toFixed(2),
+      (mouse.x / inner.offsetWidth / 2).toFixed(2)
+    );
+  };
+
+  var updateTransformStyle = function(x, y) {
+  	var x1= x*0.5;
+  	var y1 = y*0.5;
+    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    var style1 = "rotateX(" + x1 + "deg) rotateY(" + y1 + "deg)";
+    inner.style.transform = style;
+    inner.style.webkitTransform = style;
+    inner.style.mozTransform = style;
+    inner.style.msTransform = style;
+    inner.style.oTransform = style;
+    inner1.style.transform = style1;
+    inner1.style.webkitTransform = style1;
+    inner1.style.mozTransform = style1;
+    inner1.style.msTransform = style1;
+    inner1.style.oTransform = style1;
+  };
+
+  //-----------------------------------------
+
+  container.onmouseenter = onMouseEnterHandler;
+  container.onmouseleave = onMouseLeaveHandler;
+  container.onmousemove = onMouseMoveHandler;
+
+}
+
+init();
